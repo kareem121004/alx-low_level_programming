fc36eb5059fa37b0a1bcb2cd0492333081dca0a9
kareem121004 <karimadel11112004@gmail.com>
2023-07-12 23:05:37 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put c
+ *
+ * Return: Always 0 (Sucess)
+*/
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

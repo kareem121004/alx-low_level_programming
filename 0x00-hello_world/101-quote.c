e98fe24314c275c1d835d5eebb0de7168f83683e
kareem121004 <karimadel11112004@gmail.com>
2023-07-13 00:07:42 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,7 @@
 
 int main(void)
 {
-	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	int length = strlen(q);
 

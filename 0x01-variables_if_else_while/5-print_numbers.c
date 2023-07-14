5382401b17790bc80638deecf60611d11778d25e
kareem121004 <karimadel11112004@gmail.com>
2023-07-13 22:22:14 -0700

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - start point
+ *
+ * Description: Program to display all num from 0 to 9
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+
+	return (0);
+}

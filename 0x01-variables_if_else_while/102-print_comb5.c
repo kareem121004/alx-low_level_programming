9f80042aba1d67952530c04af22d839022d4cd3f
kareem121004 <karimadel11112004@gmail.com>
2023-07-14 19:38:59 -0700

    commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,16 +18,19 @@ int main(void)
 
 		while (j <= 99)
 		{
-			putchar(i / 10 + '0');
-			putchar(i % 10 + '0');
-			putchar(' ');
-			putchar(j / 10 + '0');
-			putchar(j % 10 + '0');
-
-			if (j != 99)
+			if (i != j && i < j)
 			{
-				putchar(',');
+				putchar(i / 10 + '0');
+				putchar(i % 10 + '0');
 				putchar(' ');
+				putchar(j / 10 + '0');
+				putchar(j % 10 + '0');
+
+				if (i + j != 197)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 			j++;
 		}

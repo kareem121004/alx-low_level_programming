7ea0279980341353126135eb580962cf2e9e5d21
kareem121004 <karimadel11112004@gmail.com>
2023-07-14 19:22:59 -0700

    commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - start point
+ *
+ * Description: prints all possible different combinations of two digits
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 99)
+	{
+		int j = 0;
+
+		while (j <= 99)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+
+			if (j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}

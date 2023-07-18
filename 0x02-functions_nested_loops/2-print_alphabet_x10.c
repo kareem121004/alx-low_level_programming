4cf03fded734722e9cb03a139da56338735e9a9b
kareem121004 <karimadel11112004@gmail.com>
2023-07-18 07:32:06 -0700

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
-* main - function to print alpha for 9 times
-*
-* Return: Always 0.
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
 */
 
 void print_alphabet_x10(void)

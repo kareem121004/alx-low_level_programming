1e9958c20469c6ff74b1df8b85aa9cf379d126b4
kareem121004 <karimadel11112004@gmail.com>
2023-07-18 06:44:10 -0700

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 {
 	for (int i = 0 ; i < 10 ; i++)
 	{
-		for (int c = 97 ; c <= 122 ; c++)
+		for (int c = 'a' ; c <= 'z' ; c++)
 			_putchar(c);
 		_putchar('\n');
 	}

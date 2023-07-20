c1ab93a9f56e5e6ece45fcd2dce2f26edb18a463
kareem121004 <karimadel11112004@gmail.com>
2023-07-20 09:34:31 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Draws a diagonal line on the terminal using '\'.
+ *                  If n is 0 or less, only a newline character is printed.
+ *
+ * @n: The number of times the character '\' should be printed.
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	
+	for (i = 0 ; i < n; i ++)
+	{
+		for (j = 0; j < i ; j++)
+		{
+			_putchar(' ');
+		}
+
+		_putchar('\\');
+		_putchar('\n')
+	}
+}

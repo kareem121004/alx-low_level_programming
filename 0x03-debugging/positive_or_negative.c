d2bee2a44dffbd7ee77b9f9b8b1580d9de205a2d
kareem121004 <karimadel11112004@gmail.com>
2023-07-19 19:18:58 -0700

    commit

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -3,14 +3,14 @@
 /**
  * positive_or_negative - positive or negative or zero,
  * followed by a new line
- * @n: print from this number
+ * @i: print from this number
  */
-void positive_or_negative(int n)
+void positive_or_negative(int i)
 {
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is zero\n", i);
 }

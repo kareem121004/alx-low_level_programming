36b19ec2805cfe2ef7a123d86baa0bbbbf26623d
kareem121004 <karimadel11112004@gmail.com>
2023-07-24 10:52:14 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - returns the length of a string
+ * @str: is the int to be checked
+*/
+
+void puts_half(char *str)
+{
+	int l, i;
+
+	for (l = 0; str[l] != '\0'; ++l)
+		;
+	if (l % 2 != 0)
+	{
+		for (i = (l - 1) / 2; i < l; i++)
+			putchar(str[i]);
+	}
+	else
+		for (i = l / 2; i < l; i++)
+			putchar(str[i]);
+	putchar('\n');
+}

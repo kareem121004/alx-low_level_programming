7b73e89f3ab0b9a05db21c0d8f40272b6e139a27
kareem121004 <karimadel11112004@gmail.com>
2023-07-23 22:02:26 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - returns the length of a string
+ * @s: is the int to be checked
+*/
+
+void print_rev(char *s)
+{
+	int i;
+
+	while (str[i])
+		i++;
+	while (i--)
+		_putchar(str[i]);
+
+	_putchar('\n);
+}

9aad43dad45a08c94ab2bbe8f4c44da26e5a9574
kareem121004 <karimadel11112004@gmail.com>
2023-07-26 23:11:51 -0700

    commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,34 +6,28 @@
  * Return: Pointer to the modified string.
 */
 
-#include <stdio.h>
-
 char *leet(char *str)
 {
-    char leet_chars[] = "AEOTLaeotl";
-    char leet_replacements[] = "4307143071";
-    
-    char *p = str;
+	char *p = str;
+	char leet_chars[] = "AEOTLaeotl";
+	char leet_replacements[] = "4307143071";
 
-    while (*p)
-    {
-        char *leet_p = leet_chars;
-        char *leet_r = leet_replacements;
-        
-        while (*leet_p)
-        {
-            if (*p == *leet_p)
-            {
-                *p = *leet_r;
-                break;
-            }
-            leet_p++;
-            leet_r++;
-        }
-        
-        p++;
-    }
-    
-    return str;
+	while (*p)
+	{
+		char *leet_p = leet_chars;
+		char *leet_r = leet_replacements;
+
+		while (*leet_p)
+		{
+			if (*p == *leet_p)
+			{
+				*p = *leet_r;
+				break;
+			}
+			leet_p++;
+			leet_r++;
+		}
+		p++;
+	}
+	return (str);
 }
-

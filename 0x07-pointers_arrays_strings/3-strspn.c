41e487e8cec29a7782c4e5f484b5af5d39a9a978
kareem121004 <karimadel11112004@gmail.com>
2023-07-31 02:36:44 -0700

    commit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,26 +1,25 @@
 #include "main.h"
-size_t _strspn(const char *s, const char *accept)
+
+/**
+ *_strspn - a function that locates a character in a string.
+ *@s: is the char to be checked
+ *@c: is the char to be checked
+ *Return: a pointer to the first occurrence.
+*/
+
+unsigned int _strspn(char *s, char *accept)
 {
-    size_t counter = 0;
+	unsigned int itr, jtr;
 
-    for (; *s; s++)
-    {
-        int is_matched = 0;
+	for (itr = 0; s[itr] != '\0'; itr++)
+	{
+		for (jtr = 0; accept[jtr] != s[itr]; jtr++)
+		{
+			if (accept[jtr] == '\0')
+				return (itr);
 
-        for (const char *a = accept; *a; a++)
-        {
-            if (*s == *a)
-            {
-                is_matched = 1;
-                break;
-            }
-        }
+		}
+	}
 
-        if (!is_matched)
-            break;
-
-        counter++;
-    }
-
-    return counter;
+	return (itr);
 }

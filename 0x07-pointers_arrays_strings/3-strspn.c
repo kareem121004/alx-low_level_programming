6ee7ab3a8837df02e9ba1cb220f561055be489c3
kareem121004 <karimadel11112004@gmail.com>
2023-07-31 02:27:54 -0700

    commit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,36 +1,29 @@
 #include "main.h"
 #include <stdio.h>
 
-/**
- * _strspn - a function that gets the length of a prefix substring.
- * @s: The input string to be modified.
- * @accept: char to be checked
- * Return: Pointer to the modified string.
-*/
-
 unsigned int _strspn(char *s, char *accept)
 {
-	int counter = 0;
+    int counter = 0;
 
-	while (*s != '\0')
-	{
-		int is_matched = 0;
+    while (*s != '\0')
+    {
+        int is_matched = 0;
 
-		for (char *a = accept; *a != '\0'; a++)
-		{
-			if (*s == *a)
-			{
-				is_matched = 1;
-				break;
-			}
-		}
+        for (char *a = accept; *a != '\0'; a++)
+        {
+            if (*s == *a)
+            {
+                is_matched = 1;
+                break;
+            }
+        }
 
-		if (is_matched)
-			counter++;
-		else
-			break;
-		s++;
-	}
+        if (is_matched)
+            counter++;
+        else
+            break;
+        s++;
+    }
 
-	return (counter);
+    return counter;
 }

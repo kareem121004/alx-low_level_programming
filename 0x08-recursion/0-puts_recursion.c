55cb5b813b879b02b6eda8cacc214b39d68de32a
kareem121004 <karimadel11112004@gmail.com>
2023-08-02 05:24:38 -0700

    commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a function that prints a string
+ * @s: char to be checked
+*/
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		puts("\n");
+		return;
+	}
+
+	putchar(*s);
+
+	_puts_recursion(s + 1);
+}

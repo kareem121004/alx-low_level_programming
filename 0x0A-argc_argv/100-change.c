8151fa34b9f81ab81aec21fa834c681be44c2d50
kareem121004 <karimadel11112004@gmail.com>
2023-08-04 16:34:48 -0700

    commit

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,32 +1,42 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Error\n");
-        return 1;
-    }
+/**
+ * main - prints the minimum number of coins to make change
+ * @argc: num
+ * @argv: char
+ * Return: 0 or 1
+*/
 
-    int cents = atoi(argv[1]);
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    if (cents < 0) {
-        printf("0\n");
-        return 0;
-    }
+	int cents = atoi(argv[1]);
 
-    int num_coins = 0;
-    int denominations[] = {25, 10, 5, 2, 1};
-    int num_denominations = sizeof(denominations) / sizeof(denominations[0]);
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-    for (int i = 0; i < num_denominations; i++) {
-        while (cents >= denominations[i]) {
-            cents -= denominations[i];
-            num_coins++;
-        }
-    }
+	int num_coins = 0;
+	int denominations[] = {25, 10, 5, 2, 1};
+	int i;
 
-    printf("%d\n", num_coins);
-
-    return 0;
+	for (i = 0; i < 5; i++)
+	{
+		while (cents >= denominations[i])
+		{
+			cents -= denominations[i];
+			num_coins++;
+		}
+	}
+	printf("%d\n", num_coins);
+	return (0);
 }
-

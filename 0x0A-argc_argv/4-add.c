89a816124cfd0db0d369b02df21cd235409b74e9
kareem121004 <karimadel11112004@gmail.com>
2023-08-04 16:10:27 -0700

    commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,19 +11,22 @@
 
 int main(int argc, char *argv[])
 {
-    int sum = 0;
-    char *c;
-    while (--argc) {
-        for (c = argv[argc]; *c ; c++)
-        {
-            if (*c < '0' || *c > '9')
-            {
-                printf("Error\n");
-                return (1);
-            }
-        }
-        sum += atoi(argv[argc]);
-    }
-    printf("%d", sum);
-    return (0);
+	int sum = 0;
+	char *c;
+
+	while (--argc)
+	{
+		for (c = argv[argc]; *c; c++)
+		{
+			if (*c < '0' || *c > '9')
+			{
+				return (printf("Error\n"), 1);
+			}
+
+		}
+
+		sum += atoi(argv[argc]);
+	}
+	printf("%d\n", sum);
+	return (0);
 }

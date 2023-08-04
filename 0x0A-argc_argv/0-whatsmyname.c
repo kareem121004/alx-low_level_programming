f546f0c781e9a1c97433102b4c225005cbd5f65c
kareem121004 <karimadel11112004@gmail.com>
2023-08-04 08:40:53 -0700

    arg commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,9 +7,10 @@
  * Return: 0
 */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__ ((__unused__)), char *argv[])
 {
-        (void)argc;
-        printf("%s\n", argv[0]);
-        return (0);
+	printf("%s\n", argv[0]);
+
+	return (0);
+
 }

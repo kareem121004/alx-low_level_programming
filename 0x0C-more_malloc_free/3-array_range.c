0fa2976e786c0f943413b047a2fdccc42aa7f0c7
kareem121004 <karimadel11112004@gmail.com>
2023-08-10 05:16:56 -0700

    commit

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *array_range - make  memory for an array
+ * @min: int type
+ * @max: int type
+ * Return:  return pointer to array
+ */
+
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int i, len;
+
+	len = max - min + 1;
+
+	ptr = malloc(sizeof(int) * len);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		ptr[i] = min++;
+
+	return (ptr);
+}

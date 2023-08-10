c570bbb22d6d586550d1344a18ce7c51c01622f2
kareem121004 <karimadel11112004@gmail.com>
2023-08-10 04:58:35 -0700

    commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,8 +13,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *conct;
-	int i, l = 0;
-	unsigned int len1, len2;
+	unsigned int len1, len2, i, j;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -32,11 +31,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 
 	for (i = 0; s1[i] != '\0'; i++)
-		conct[l++] = s1[i];
+		conct[i] = s1[i];
 
-	for (i = 0; i < n; i++)
-		conct[l++] = s2[i];
-
-	conct[l] = '\0';
+	for (j = 0; j < n; j++)
+	{
+		conct[i] = s2[j];
+		i++;
+	}
+	conct[i] = '\0';
 	return (conct);
 }

7252a330317efba1a014cf18b0162f4bf7156d40
kareem121004 <karimadel11112004@gmail.com>
2023-08-16 09:08:28 -0700

    commmit

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,14 +21,10 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (i < 5)
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 	{
-		if (s && s[0] == ops[i].op[0] && !s[1])
-		{
-			return (ops[i].f);
-		}
 		i++;
 	}
 
-	return (NULL);
+	return (ops[i].f);
 }

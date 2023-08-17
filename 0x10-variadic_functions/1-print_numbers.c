3ee9b67eedaa8e871ccc3b6733f682c69f50da54
kareem121004 <karimadel11112004@gmail.com>
2023-08-17 08:43:49 -0700

    commmit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,11 +16,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		return;
 	va_start(args, n);
 
-	for (i = 1; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d",va_arg(args, int));
 
-		if (i < n)
+		if (i < n - 1)
 			printf("%s", separator);
 	}
 	va_end(args);

6cff71572b981914d3676d8fce91bb9104c3f09c
kareem121004 <karimadel11112004@gmail.com>
2023-08-29 15:45:39 -0700

    commit with hope

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,18 +9,18 @@
 
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev = NULL;
-	listint_t *current = *head;
-	listint_t *nextnode;
+    listint_t *prev = NULL;
+    listint_t *next = NULL;
+    listint_t *current = *head;
 
-	while (current != NULL)
-	{
-		nextnode = current->next;
-		current->next = prev;
-		prev = current;
-		current = nextnode;
-	}
+    while (current != NULL) {
+        next = current->next;   
+        current->next = prev;
+        prev = current;     
+        current = next;       
+    }
 
-	*head = prev;
-	return (*head);
+    *head = prev;
+
+    return *head;
 }

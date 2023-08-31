b19dc7afd67f79d85f2a1ac90831870ac8419c84
kareem121004 <karimadel11112004@gmail.com>
2023-08-31 09:00:00 -0700

    commit new

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+
+	if (!b)
+		return (0);
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+		result = (result << 1) + (*b - '0');
+		b++;
+	}
+	return (result);
+}

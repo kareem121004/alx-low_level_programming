4762162c11de8590f501c55b04556e1c506bc7b4
kareem121004 <karimadel11112004@gmail.com>
2023-09-04 12:32:29 -0700

    h

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -31,33 +31,30 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 	char buffer[BUFFER_SIZE];
-	ssize_t bytes_read, bytes_written;
+	ssize_t bytes_read;
 
 	while ((bytes_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		bytes_written = write(file_to, buffer, bytes_read);
-		if (bytes_written == -1)
+		if (write(file_to, buffer, bytes_read) != bytes_read)
 		{
 			dprintf(2, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
-		}
-		if (bytes_written != bytes_read)
-		{
-			dprintf(2, "Error: Write error while copying data\n");
-			exit(99);
-		}
+		}	
 	}
 	if (bytes_read == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (close(file_from) == -1)
+
+	file_from = close(file_from);
+	file_to = close(file_to);
+	if (file_from)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
-	if (close(file_to) == -1)
+	if (file_to)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", file_to);
 		exit(100);

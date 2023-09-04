54cce8b778c6d23bda8cba010afce159eda45c50
kareem121004 <karimadel11112004@gmail.com>
2023-09-04 12:41:54 -0700

    ho

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,17 +16,12 @@ int main(int argc, char *argv[])
 	ssize_t bytes_read;
 
 	if (argc != 3)
-	{
-		 dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
-		 exit(97);
-	}
+		dprintf(2, "Usage: %s file_from file_to\n", argv[0]), exit(97);
+
 	file_from = open(argv[1], O_RDONLY);
 
 	if (file_from == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
 	if (file_to == -1)
@@ -40,25 +35,17 @@ int main(int argc, char *argv[])
 		{
 			dprintf(2, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
-		}	
+		}
 	}
 	if (bytes_read == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
 
 	file_from = close(file_from);
 	file_to = close(file_to);
 	if (file_from)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
+		dprintf(2, "Error: Can't close fd %d\n", file_from), exit(100);
 	if (file_to)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", file_to);
-		exit(100);
-	}
+		dprintf(2, "Error: Can't close fd %d\n", file_to), exit(100);
+
 	return (0);
 }

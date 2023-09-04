1262a5689fb9a4b59e15f7e46c40a5ddf74fcbf9
kareem121004 <karimadel11112004@gmail.com>
2023-09-04 12:57:16 -0700

    commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,11 @@
 #include "main.h"
 
 
+#define ERROR_ARG_COUNT "Usage: %s file_from file_to\n"
+#define ERROR_READ_FILE "Error: Can't read from file %s\n"
+#define ERROR_WRITE_FILE "Error: Can't write to %s\n"
+#define ERROR_CLOSE_FD "Error: Can't close fd %d\n"
+
 /**
  * main - Copies the contents of a file to another file.
  * @argc: The number of arguments supplied to the program.
@@ -15,32 +20,32 @@ int main(int argc, char *argv[])
 	ssize_t bytes_read;
 
 	if (argc != 3)
-		dprintf(2, "Usage: %s file_from file_to\n", argv[0]), exit(97);
+		dprintf(2, ERROR_ARG_COUNT, argv[0]), exit(97);
 
 	file_from = open(argv[1], O_RDONLY);
 
 	if (file_from == -1)
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		dprintf(2, ERROR_READ_FILE , argv[1]), exit(98);
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
 	if (file_to == -1)
-		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+		dprintf(2, ERROR_WRITE_FILE, argv[2]), exit(99);
 
 	while ((bytes_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		if (write(file_to, buffer, bytes_read) != bytes_read)
-			dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+			dprintf(2, ERROR_WRITE_FILE, argv[2]), exit(99);
 	}
 
 	if (bytes_read == -1)
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		dprintf(2, ERROR_READ_FILE, argv[1]), exit(98);
 
 	file_from = close(file_from);
 	file_to = close(file_to);
 	if (file_from)
-		dprintf(2, "Error: Can't close fd %d\n", file_from), exit(100);
+		dprintf(2, ERROR_CLOSE_FD, file_from), exit(100);
 	if (file_to)
-		dprintf(2, "Error: Can't close fd %d\n", file_to), exit(100);
+		dprintf(2, ERROR_CLOSE_FD, file_to), exit(100);
 
 	return (0);
 }

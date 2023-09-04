d143a53eea08da9c27252db362d04bb53d46a09f
kareem121004 <karimadel11112004@gmail.com>
2023-09-04 14:00:25 -0700

    commit ne

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,55 +1,50 @@
 #include "main.h"
 
-#define BUFFER_SIZE 1024
 
-#define ERROR_ARG_COUNT "Usage: cp file_from file_to\n"
+#define ERROR_ARG_COUNT "Usage: cp file_from file_to"
 #define ERROR_READ_FILE "Error: Can't read from file %s\n"
 #define ERROR_WRITE_FILE "Error: Can't write to %s\n"
 #define ERROR_CLOSE_FD "Error: Can't close fd %d\n"
 
-int main(int argc, char **argv) {
-    if (argc != 3) {
-        dprintf(STDERR_FILENO, ERROR_ARG_COUNT);
-        exit(97);
-    }
+/**
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: 0 on success.
+*/
 
-    int file_from, file_to;
-    ssize_t bytes_read;
-    char buffer[BUFFER_SIZE];
+int main(int argc, char **argv)
+{
+	int file_from, file_to;
+	ssize_t bytes_read;
+	char buffer[BUFFER_SIZE];
 
-    file_from = open(argv[1], O_RDONLY);
-    if (file_from == -1) {
-        dprintf(STDERR_FILENO, ERROR_READ_FILE, argv[1]);
-        exit(98);
-    }
+	if (argc != 3)
+		dprintf(STDERR_FILENO, ERROR_ARG_COUNT), exit(97);
 
-    file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-    if (file_to == -1) {
-        dprintf(STDERR_FILENO, ERROR_WRITE_FILE, argv[2]);
-        exit(99);
-    }
+	file_from = open(argv[1], O_RDONLY);
 
-    while ((bytes_read = read(file_from, buffer, BUFFER_SIZE)) > 0) {
-        ssize_t bytes_written = write(file_to, buffer, bytes_read);
-        if (bytes_written == -1 || bytes_written != bytes_read) {
-            dprintf(STDERR_FILENO, ERROR_WRITE_FILE, argv[2]);
-            exit(99);
-        }
-    }
+	if (file_from == -1)
+		dprintf(STDERR_FILENO, ERROR_READ_FILE, argv[1]), exit(98);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-    if (bytes_read == -1) {
-        dprintf(STDERR_FILENO, ERROR_READ_FILE, argv[1]);
-        exit(98);
-    }
+	if (file_to == -1)
+		dprintf(STDERR_FILENO, ERROR_WRITE_FILE, argv[2]), exit(99);
 
-    if (close(file_from) == -1) {
-        dprintf(STDERR_FILENO, ERROR_CLOSE_FD, file_from);
-        exit(100);
-    }
-    if (close(file_to) == -1) {
-        dprintf(STDERR_FILENO, ERROR_CLOSE_FD, file_to);
-        exit(100);
-    }
+	while ((bytes_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		if (write(file_to, buffer, bytes_read) != bytes_read)
+			dprintf(STDERR_FILENO, ERROR_WRITE_FILE, argv[2]), exit(99);
+	}
 
-    return 0;
+	if (bytes_read == -1)
+		dprintf(STDERR_FILENO, ERROR_READ_FILE, argv[1]), exit(98);
+	file_from = close(file_from);
+	file_to = close(file_to);
+	if (file_from)
+		dprintf(STDERR_FILENO, ERROR_CLOSE_FD, file_from), exit(100);
+	if (file_to)
+		dprintf(STDERR_FILENO, ERROR_CLOSE_FD, file_from), exit(100);
+
+	return (0);
 }

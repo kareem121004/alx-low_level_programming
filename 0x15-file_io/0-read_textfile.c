0c800687903230e2afdad5a8f154e359aea344bd
kareem121004 <karimadel11112004@gmail.com>
2023-09-04 09:26:15 -0700

    commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,10 +16,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	fo = open(filename, O_RDONLY);
 
-	if (fo == NULL)
+	if (fo == -1)
 		return (0);
 	buf = malloc(letters);
 
+	if (buff == NULL)
+	{
+		close(fo);
+		return (0);
+	}
 	bytes_read = read(fo, buf, letters);
 	w = write(STDOUT_FILENO, buf, bytes_read);
 
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 

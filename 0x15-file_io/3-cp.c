69890da5eb1089bba85270d7eb0fe9dc4a23a333
kareem121004 <karimadel11112004@gmail.com>
2023-09-04 12:45:52 -0700

    ho

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,18 +25,14 @@ int main(int argc, char *argv[])
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
 	if (file_to == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+
 	while ((bytes_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		if (write(file_to, buffer, bytes_read) != bytes_read)
-		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
+			dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
+
 	if (bytes_read == -1)
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
 

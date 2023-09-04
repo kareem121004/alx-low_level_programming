6496929b56c0ab81a798f6747bb7138e98941793
kareem121004 <karimadel11112004@gmail.com>
2023-09-04 13:00:51 -0700

    commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,32 +20,32 @@ int main(int argc, char *argv[])
 	ssize_t bytes_read;
 
 	if (argc != 3)
-		dprintf(2, ERROR_ARG_COUNT, argv[0]), exit(97);
+		dprintf(STDERR_FILENO, ERROR_ARG_COUNT, argv[0]), exit(97);
 
 	file_from = open(argv[1], O_RDONLY);
 
 	if (file_from == -1)
-		dprintf(2, ERROR_READ_FILE , argv[1]), exit(98);
+		dprintf(STDERR_FILENO, ERROR_READ_FILE, argv[1]), exit(98);
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
 	if (file_to == -1)
-		dprintf(2, ERROR_WRITE_FILE, argv[2]), exit(99);
+		dprintf(STDERR_FILENO, ERROR_WRITE_FILE, argv[2]), exit(99);
 
 	while ((bytes_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		if (write(file_to, buffer, bytes_read) != bytes_read)
-			dprintf(2, ERROR_WRITE_FILE, argv[2]), exit(99);
+			dprintf(STDERR_FILENO, ERROR_WRITE_FILE, argv[2]), exit(99);
 	}
 
 	if (bytes_read == -1)
-		dprintf(2, ERROR_READ_FILE, argv[1]), exit(98);
+		dprintf(STDERR_FILENO, ERROR_READ_FILE, argv[1]), exit(98);
 
 	file_from = close(file_from);
 	file_to = close(file_to);
 	if (file_from)
-		dprintf(2, ERROR_CLOSE_FD, file_from), exit(100);
+		dprintf(STDERR_FILENO, ERROR_CLOSE_FD, file_from), exit(100);
 	if (file_to)
-		dprintf(2, ERROR_CLOSE_FD, file_to), exit(100);
+		dprintf(STDERR_FILENO, ERROR_CLOSE_FD, file_to), exit(100);
 
 	return (0);
 }

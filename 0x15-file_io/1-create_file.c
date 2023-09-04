26f65c56b52e35769dc42d0f6af1f9873332b6d0
kareem121004 <karimadel11112004@gmail.com>
2023-09-04 09:41:53 -0700

    commit ne

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,12 +9,17 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fo, w;
+	int fo, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 	fo = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	w = write(fo, text_content, strlen(text_content));
+	w = write(fo, text_content, len);
 	if (fo == -1 || w == -1)
 		return (-1);
 	close(fo);

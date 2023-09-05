2fb4b32ae8c5cee3f0ee85d45e866d7ac61a7629
kareem121004 <karimadel11112004@gmail.com>
2023-09-04 18:23:38 -0700

    commit

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - displays the information contained in the ELF
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: 0 on success.
+*/
+
+int main(int argc, char **argv)
+{
+
+}

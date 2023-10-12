1be3dbabc420a7900b4f7fe47a44b5461dbcda6e
kareem121004 <karimadel11112004@gmail.com>
2023-10-12 09:15:15 -0700

    commit again

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,13 +16,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *temp;
 
 	temp = *h;
-	newnode = malloc(sizeof(dlistint_t));
-
-	if (newnode == NULL)
-		return (NULL);
-
-	newnode->n = n;
-
 	if (idx == 0)
 		return (add_dnodeint_end(h, n));
 
@@ -37,6 +30,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (temp->next == NULL)
 		return (add_dnodeint_end(h, n));
 
+	newnode = malloc(sizeof(dlistint_t));
+
+	if (newnode == NULL)
+		return (NULL);
+
+	newnode->n = n;
 	newnode->prev = temp;
 	newnode->next = temp->next;
 	temp->next->prev = newnode;

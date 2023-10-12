71b32be606d35d085a0d9dcb67d83c58713f02e1
kareem121004 <karimadel11112004@gmail.com>
2023-10-12 07:50:30 -0700

    commit new

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - calculates the sum of all the data in a listint_t list
+ * @head: first node in the linked list
+ * Return: resulting sum
+*/
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	dlistint_t *temp;
+
+	temp head;
+
+	while (temp)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return (sum);
+}

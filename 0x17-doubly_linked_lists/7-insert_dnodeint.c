85f68c543f181e4b804c0628643d01fb2671f625
kareem121004 <karimadel11112004@gmail.com>
2023-10-12 09:10:22 -0700

    commit new

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -38,6 +38,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	newnode->n = n;
 	newnode->prev = temp;
 	newnode->next = temp->next;
+	temp->next->prev = newnode;
 	temp->next = newnode;
 
 	return (newnode);

82352f49241e926755be651b8691a8b3e7f3b917
kareem121004 <karimadel11112004@gmail.com>
2023-11-03 02:36:11 -0700

    del

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -19,7 +19,7 @@ void hash_table_delete(hash_table_t *ht)
 
 		while (current != NULL)
 		{
-			temp = current;
+			tmp = current;
 			current = current->next;
 			free(tmp->key);
 			free(tmp->value);
